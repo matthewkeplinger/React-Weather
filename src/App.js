532fecd4073155ae9818a0f25d74bcfd182ea938
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-11-02 10:23:08 -0400

    Project Initial Commit; Styling complete; need logic

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+//Declare API key as constant
+const api = {
+  key: "f196986934f2f378cdcfc78b3a90a97a",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className = 'app'>
+        <main>
+          <div className = 'search-box'>
+            <input 
+              type = 'text' 
+              className = 'search-bar' 
+              placeholder = 'Search...'
+            />
+          </div>
+          <div className = 'location-box'>
+            <div className = 'location'>New York City, USA</div>
+            <div className = 'date'>{dateBuilder(new Date())}</div>
+          </div>
+        </main>
+    </div>
+  );
+}
+
+export default App;
